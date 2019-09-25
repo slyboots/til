12a02c7d9a87a838016dc692211eb945e1c88075
Dakota Wisdom Lorance <dakota@likethestates.com>
2019-09-25 10:46:09 -0500

    can pass date to new_post script now

--- new_post.sh
+++ new_post.sh
@@ -1,19 +1,23 @@
 #! /usr/local/bin/bash
 
-if [[ "$#" -ne 1 ]]; then
+if [[ ! "$#" -gt 1 ]]; then
     echo "No title provided";
 else
     if [[ ! -d _posts ]]; then
         mkdir _posts;
     fi
-
+    # get title for post
     title="$1";
-    postdate=$(date +%Y-%m-%d);
+
+    # use date provided for post otherwise default to current date
+    currentdate=$(date +%Y-%m-%d);
+    postdate="${2:-$currentdate}";
+
     postname=$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed 's/\s/-/g');
     postfilename="${postdate}-${postname}.md";
 
     posttitle=$(echo "$title" | sed 's/.*/\L&/; s/[a-z]*/\u&/g');
-    posttemplate="---\nlayout: post\ntitle: \"${posttitle}\"\nstuff:\n\t-\n---\n\n";
+    posttemplate="---\nlayout: post\ntitle: \"${posttitle}\"\nthings:\n\t-\n---\n\n";
 
     echo -e "${posttemplate}" > "_posts/$postfilename";
 
